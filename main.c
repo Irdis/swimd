884842f15a8b079b4deaacbd63cb4433ceb07fc2
Ivan Novitskii <ivannovitskii@gmail.com>
2025-10-24 01:50:07 +0100

    prep vectors

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 #define MAX_PATH_LENGTH 300
 #define LANES_COUNT_SHORT 16
+#define GAP_PENALTY 3
+#define SUB_PENALTY 2
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
@@ -46,10 +48,13 @@ typedef struct {
 
 typedef struct {
     char* needle;
+    int needle_length;
     short* needle_vec;
+    int needle_vec_length;
 
     Swimd_File_List files;
     Swimd_File_Vec* files_vec;
+    int files_vec_length;
 
     Swimd_Folder_Struct folders;
     short* scores;
@@ -218,6 +223,59 @@ void swimd_prep_files_vec(Swimd_Algo_State* state) {
         };
     }
     state->files_vec = files_vec;
+    state->files_vec_length = files_vec_length;
+}
+
+void swimd_prep_files_vec_free(Swimd_Algo_State* state) {
+    for (int i = 0; i < state->files_vec_length; i++) {
+        Swimd_File_Vec file_vec = state->files_vec[i];
+        free(file_vec.arr);
+    }
+    free(state->files_vec);
+}
+
+void swimd_prep_needle_vec(Swimd_Algo_State* state) {
+    int needle_vec_length = state->needle_length * LANES_COUNT_SHORT;
+    short* needle_vec = malloc(needle_vec_length * sizeof(short));
+    for (int i = 0; i < state->needle_length; i++) {
+        for (int j = 0; j < LANES_COUNT_SHORT; j++) {
+            needle_vec[i * LANES_COUNT_SHORT + j] = (short)state->needle[i];
+        }
+    }
+    state->needle_vec = needle_vec;
+    state->needle_vec_length = needle_vec_length;
+}
+
+void swimd_prep_needle_vec_free(Swimd_Algo_State* state) {
+    free(state->needle_vec);
+}
+
+void swimd_setup_needle(char* needle) {
+    swimd_state.needle = needle;
+    swimd_state.needle_length = strlen(needle);
+    swimd_prep_needle_vec(&swimd_state);
+}
+
+void swimd_setup_needle_free() {
+    swimd_prep_needle_vec_free(&swimd_state);
+}
+
+void swimd_prep_d_vec(Swimd_Algo_State* state) {
+    memset(state->d_vec, 0, MAX_PATH_LENGTH * MAX_PATH_LENGTH * LANES_COUNT_SHORT * sizeof(short));
+    for (int i = 1; i < MAX_PATH_LENGTH; i++) {
+        short value = state->d_vec[(i - 1) * MAX_PATH_LENGTH * LANES_COUNT_SHORT];
+        value -= GAP_PENALTY;
+        for (int j = 0; j < LANES_COUNT_SHORT; j++) {
+            state->d_vec[i * MAX_PATH_LENGTH * LANES_COUNT_SHORT + j] = value;
+        }
+    }
+    for (int i = 1; i < MAX_PATH_LENGTH; i++) {
+        short value = state->d_vec[(i - 1) * LANES_COUNT_SHORT];
+        value -= GAP_PENALTY;
+        for (int j = 0; j < LANES_COUNT_SHORT; j++) {
+            state->d_vec[i * LANES_COUNT_SHORT + j] = value;
+        }
+    }
 }
 
 void swimd_vec_sum() {
@@ -236,48 +294,35 @@ void swimd_vec_sum() {
     printf("\n");
 }
 
-void swimd_init_state(const char* root_path) {
+void swimd_state_init(const char* root_path) {
     swimd_filelist_init(&swimd_state.files);
     swimd_folders_init(&swimd_state.folders.folder_lst);
     swimd_list_directories(root_path, 
         &swimd_state.files, 
         &swimd_state.folders);
 
-    printf("over %d\n", swimd_state.files.length);
     swimd_prep_files_vec(&swimd_state);
+    swimd_prep_d_vec(&swimd_state);
+}
 
-    // for (int i = 0; i < swimd_state.files.length; i++) {
-    //     printf("%d |%s|\n", i, swimd_state.files.arr[i].name);
-    // }
-    printf("over %d\n", swimd_state.files.length);
-    // for (int i = 0; i < swimd_state.files.length / LANES_COUNT_SHORT; i++) {
-    //     printf("%d |%s|\n", i, swimd_state.files.arr[i].name);
-    // }
-    // printf("over %d\n", swimd_state.files.length);
+void swimd_state_free() {
+    swimd_prep_files_vec_free(&swimd_state);
+    swimd_list_directories_free(&swimd_state.files, 
+            &swimd_state.folders);
+    swimd_folders_free(&swimd_state.folders.folder_lst);
+    swimd_filelist_free(&swimd_state.files);
 }
 
 int main() {
     const char* root_path = "c:\\temp";
-    swimd_init_state(root_path);
-    // Swimd_File_List lst = {0};
-    // Swimd_Folder_Struct root = {0};
-    //
-    // swimd_filelist_init(&lst);
-    // swimd_folders_init(&root.folder_lst);
-    // const char* root_path = "c:\\temp";
-    // swimd_list_directories(root_path, &lst, &root);
-    //
-    // swimd_prep_files_vec(&swimd_state);
-    //
-    // for (int i = 0; i < lst.length; i++) {
-    //     printf("%d |%s|\n", i, lst.arr[i].name);
-    // }
-    // printf("over %d\n", lst.length);
-    //
-    // swimd_list_directories_free(&lst, &root);
-    // swimd_folders_free(&root.folder_lst);
-    //
-    // swimd_filelist_free(&lst);
+    while (1) {
+        swimd_state_init(root_path);
+        swimd_setup_needle("hello");
 
+        swimd_setup_needle_free();
+        swimd_state_free();
+
+        getchar();
+    }
     return 0;
 }
