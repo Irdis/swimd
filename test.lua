f52a52ca716dabade0e773d5f93db1e2ca4072b2
Ivan Navitski <ivannovitskii@gmail.com>
2026-02-02 23:24:15 +0000

    move to different path

--- test.lua
+++ test.lua
@@ -3,6 +3,7 @@ Swimd = require("swimd")
 print(Swimd.say_hello("abc1"))
 Swimd.init()
 Swimd.setup_workspace("c:\\projects")
+Swimd.refresh_workspace()
 print(vim.inspect(Swimd.process_input("main", 5)))
 Swimd.shutdown()
 
